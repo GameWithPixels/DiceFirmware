53dcd99375f283fe9466566c0ef244c5bb2954e5
Olivier Basille <obasille@hotmail.com>
2021-11-23 18:51:39 +0100

    Change version info to 10 characters

--- src/bluetooth/bluetooth_messages.h
+++ src/bluetooth/bluetooth_messages.h
@@ -6,7 +6,7 @@
 #include "modules/accelerometer.h"
 
 #define MAX_DATA_SIZE 100
-#define VERSION_INFO_SIZE 6
+#define VERSION_INFO_SIZE 10
 
 #pragma pack(push, 1)
 
@@ -112,7 +112,7 @@ struct MessageIAmADie
 	uint32_t dataSetHash;
 	uint32_t deviceId; // A unique identifier
 	uint16_t flashSize; // How much room available for data
-	char versionInfo[VERSION_INFO_SIZE]; // Firmware version string, i.e. "10_05"
+	char versionInfo[VERSION_INFO_SIZE]; // Firmware version string, i.e. "10_05_20"
 	inline MessageIAmADie() : Message(Message::MessageType_IAmADie) { versionInfo[0] = 0; }
 };
 
