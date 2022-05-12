e6d057dfcc2010cfb9585e61fc73ccba987e4562
Kyle Zerner <kylzrnr@gmail.com>
2022-05-12 12:43:40 -0500

    Create generic accelerometer header file

---
+++ src/drivers_hw/accel_chip.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace Core
+{
+    struct float3;
+}
+
+namespace DriversHW
+{
+	/// <summary>
+	/// The accelerometer I2C devices
+	/// </summary>
+	namespace AccelChip
+	{
+		void init();
+		void read(Core::float3* outAccel);
+
+		void enableInterrupt();
+        void enableDataInterrupt();
+		void disableInterrupt();
+		void disableDataInterrupt();
+		void clearInterrupt();
+
+		void lowPower();
+
+		// Notification management
+		typedef void(*AccelClientMethod)(void* param, const Core::float3& acceleration);
+		void hook(AccelClientMethod method, void* param);
+		void unHook(AccelClientMethod client);
+		void unHookWithParam(void* param);
+	}
+}
+
