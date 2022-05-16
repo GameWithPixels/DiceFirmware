bf2f9ba2e2fc8b98b1d031178edcce0dd5b2d2bc
Olivier Basille <obasille@hotmail.com>
2022-05-16 17:53:05 +0200

    Hook to RequestRollState message in validation mode too and rename DieState message to RollState
    Remove unused initDebugLogic()
    
    (cherry picked from commit 38fc2e8fb3f41af83fab905558a16395492fdbd1)

--- src/bluetooth/bluetooth_messages.cpp
+++ src/bluetooth/bluetooth_messages.cpp
@@ -9,8 +9,8 @@ namespace Bluetooth
 		{
 		case MessageType_None:
 			return "None";
-		case MessageType_State:
-			return "State";
+		case MessageType_RollState:
+			return "RollState";
 		case MessageType_Telemetry:
 			return "Telemetry";
 		case MessageType_BulkSetup:
@@ -33,8 +33,8 @@ namespace Bluetooth
 			return "DebugLog";
 		case MessageType_PlayAnim:
 			return "PlayAnim";
-		case MessageType_RequestState:
-			return "RequestState";
+		case MessageType_RequestRollState:
+			return "RequestRollState";
 		case MessageType_RequestAnimSet:
 			return "RequestAnimSet";
 		case MessageType_RequestSettings:
--- src/bluetooth/bluetooth_messages.h
+++ src/bluetooth/bluetooth_messages.h
@@ -23,7 +23,7 @@ struct Message
 		MessageType_None = 0,
 		MessageType_WhoAreYou,
 		MessageType_IAmADie,
-		MessageType_State,
+		MessageType_RollState,
 		MessageType_Telemetry,
 		MessageType_BulkSetup,
 		MessageType_BulkSetupAck,
@@ -44,7 +44,7 @@ struct Message
 		MessageType_PlayAnimEvent,
 		MessageType_StopAnim,
 		MessageType_PlaySound,
-		MessageType_RequestState,
+		MessageType_RequestRollState,
 		MessageType_RequestAnimSet,
 		MessageType_RequestSettings,
 		MessageType_RequestTelemetry,
@@ -124,13 +124,13 @@ struct MessageIAmADie
 /// <summary>
 /// Describes a state change detection message
 /// </summary>
-struct MessageDieState
+struct MessageRollState
 	: public Message
 {
 	uint8_t state;
 	uint8_t face;
 
-	inline MessageDieState() : Message(Message::MessageType_State) {}
+	inline MessageRollState() : Message(Message::MessageType_RollState) {}
 };
 
 /// <summary>
