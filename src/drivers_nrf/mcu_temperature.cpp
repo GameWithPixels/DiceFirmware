0bbaa7ad359baa7e795844d68c61aa6cb2fac8b8
Olivier Basille <obasille@hotmail.com>
2022-12-19 12:26:00 +0100

    Add Temperature::measure() function, schedule processing temp reading

--- src/drivers_nrf/mcu_temperature.cpp
+++ src/drivers_nrf/mcu_temperature.cpp
@@ -36,30 +36,53 @@ namespace DriversNRF::MCUTemperature
         NRF_LOG_INFO("Temperature Initialized");
     }
 
+    bool measure() {
+        ret_code_t ret = nrfx_temp_measure();
+        return ret == NRF_SUCCESS;
+    }
+
     void temperatureReadyHandler(int32_t raw_measurement) {
         int celsiusTimes100 = nrfx_temp_calculate(raw_measurement);
-		for (int i = 0; i < clients.Count(); ++i) {
-			clients[i].handler(clients[i].token, celsiusTimes100);
-		}
+        Scheduler::push(&celsiusTimes100, sizeof(int), [](void *tempTimes100Ptr, uint16_t event_size) {
+            int the_tempTimes100 = *(int*)tempTimes100Ptr;
+            for (int i = 0; i < clients.Count(); ++i) {
+                clients[i].handler(clients[i].token, the_tempTimes100);
+            }
+        });
     }
 
     void getTemperatureHandler(const Message* msg) {
+        // We don't want to register to the clients list multiple times.
+        // It would happen when we get several Temp request messages before 
+        // having the time to send the response.
+        // In this case sending just one response for all the pending requests
+        // is fine.
+        static bool hasPendingRequest = false;
+        if (hasPendingRequest)
+            return;
+            
         // Since reading temperature takes times and uses our interrupt handler, register a lambda
         // and we'll unregister it once we have a result.
-        NRF_LOG_INFO("Received Temperature Request");
+        NRF_LOG_INFO("Received Temp Request");
         void* uniqueToken = (void*)0x1234;
         ret_code_t ret = nrfx_temp_measure();
         if (ret == NRF_SUCCESS) {
-            clients.Register(uniqueToken, [] (void* the_uniquetoken, int the_temp) {
-                NRF_LOG_INFO("Sending temperature: %d.%d C", (the_temp / 100), (the_temp % 100));
+            if (clients.Register(uniqueToken, [] (void* the_uniquetoken, int the_tempTimes100) {
+                NRF_LOG_INFO("Sending temp: %d.%d C", (the_tempTimes100 / 100), (the_tempTimes100 % 100));
                 clients.UnregisterWithToken(the_uniquetoken);
 
                 // Send message back
                 MessageTemperature tmp;
-                tmp.mcuTempTimes100 = the_temp;
+                tmp.mcuTempTimes100 = the_tempTimes100;
                 tmp.batteryTempTimes100 = (uint16_t)(NTC::getNTCTemperature() * 100.0f);
                 MessageService::SendMessage(&tmp);
-            });
+
+                // The response message was send, allow ourselves to register
+                // to the clients list again on the next temp request
+                hasPendingRequest = false;
+            })) {
+                hasPendingRequest = true;
+            }
         } else {
             // Send message back
             MessageTemperature tmp;
--- src/drivers_nrf/mcu_temperature.h
+++ src/drivers_nrf/mcu_temperature.h
@@ -4,6 +4,9 @@ namespace DriversNRF::MCUTemperature
 {
     void init();
 
+    // Trigger a temperature measurement, use the hook() to get the response
+    bool measure();
+
     // Notification management
     typedef void(*TemperatureClientMethod)(void* param, int temperatureTimes100);
     void hook(TemperatureClientMethod method, void* param);
